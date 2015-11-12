3d88ef88c4d6edc2fecfedcb653236b75cf2296e
Adam Caron <adamwcaron@gmail.com>
2015-11-11 19:32:08 -0700

    Create a schedule object with time slots

---
+++ app/scripts/constants/schedules.js
@@ -0,0 +1,61 @@
+// module.exports = [];
+
+// to create the unique id,
+// take the data we're about to persist,
+// append the key (an ENV var) ...
+// and 'sha2' it (or whatever)
+
+// var sched = schedules[0]
+// sched.
+
+// ruby .secure_random
+
+// let schedules = {
+//   '234l5kj': {
+//     dashboardUrl: '43j2k6', // http://schedulator.com/:id/43j2k6 => http://schedulator.com/:id
+//     slots: [
+//       {
+//         date: '10/1/16',
+//         startTime: '',
+//         endTime: '',
+//         user: 'Jon Doe'
+//       },
+//       {
+//         date: '10/1/16',
+//         startTime: '',
+//         endTime: '',
+//         user: null
+//       },
+//       {
+//         date: '10/1/16',
+//         startTime: '',
+//         endTime: '',
+//         user: null
+//       }
+//     ]
+//   },
+//   '32jk54h': {
+//     url: '234l5kj',
+//     dashboardUrl: '43j2k6',
+//     slots: [
+//       {
+//         date: '10/1/16',
+//         startTime: '',
+//         endTime: '',
+//         user: null
+//       },
+//       {
+//         date: '10/1/16',
+//         startTime: '',
+//         endTime: '',
+//         user: null
+//       },
+//       {
+//         date: '10/1/16',
+//         startTime: '',
+//         endTime: '',
+//         user: null
+//       }
+//     ]
+//   }
+// }
